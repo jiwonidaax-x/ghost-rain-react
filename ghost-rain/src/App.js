be63b7171ea7f1c899ced55706158f6a9a984c04
jiwonidaax-x <jgb07012@gmail.com>
2022-10-02 18:30:15 -0400

    tailwind css

--- ghost-rain/src/App.js
+++ ghost-rain/src/App.js
@@ -1,25 +1,21 @@
-// import logo from './logo.svg';
-// import './App.css';
+function App() {
+  return (
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+    <div id="container"className="bg-green-900">
+      <div id="title">
+        <h1>Enemy Rain</h1>
+      </div>
+      <div id="buttons">
+      <button id="desc" >
+          walk-through
+        </button> 
+        <button id="game-start">
+          game start
+        </button>
+      </div>
+    </div>
 
-// export default App;
+  );
+}
+
+export default App;
