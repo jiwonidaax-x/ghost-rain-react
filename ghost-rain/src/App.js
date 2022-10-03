f788f2660e4bea01988d29053d81a0315e58319f
jiwonidaax-x <jgb07012@gmail.com>
2022-10-02 21:03:30 -0400

    button

--- ghost-rain/src/App.js
+++ ghost-rain/src/App.js
@@ -8,7 +8,7 @@ function App() {
           <Button name="Walk-through"/>
           <Button name="Game start"/>
       </div>
-      </>
+    </>
   );
 }
 
--- ghost-rain/src/Button.js
+++ ghost-rain/src/Button.js
@@ -1,8 +1,8 @@
 function Button(file){
     return(
-    <button className='bg-[#8e7964] ml-5 mr-5 p-4 h-[50px] w-[250px]'>
-     {file.name}
-    </button>
+        <button className='bg-[#8e7964] ml-5 mr-5 px-4 h-[50px] w-[250px] text-lg rounded-md text-white'>
+        {file.name}
+        </button>
     );
 }
 export default Button; 
