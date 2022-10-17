911728df96884d8389a2e28de90f93680e168c44
jiwonidaax-x <jgb07012@gmail.com>
2022-10-17 04:44:03 -0400

    route

--- ghost-rain/src/Button.js
+++ ghost-rain/src/Button.js
@@ -1,11 +1,9 @@
 import { useNavigate } from "react-router-dom";
 function Button({value}){
     const navigate=useNavigate();
-
     const goStart=()=>{
      value==="Start"? navigate('/start'):navigate('/desc');
     }
-
  return(
         <button onClick={goStart} className='bg-[#8e7964] ml-5 mr-5 px-4 h-[50px] w-[250px] text-lg rounded-md text-white'>
         {value}
